3ddfb3884dec5859263369d2a77b0f20fdd27ba3
Claudio Campuzano <claudio.campuzano.b@gmail.com>
2021-12-28 13:20:29 -0300

    Update scripts.sql

--- scripts.sql
+++ scripts.sql
@@ -67,6 +67,41 @@ begin
 end;
 $$
 
+create or replace function logds_toinflux_1() 
+	returns table (
+		id_cc int,
+  		id_ds int,
+		fps character varying,
+    	datetime timestamp with time zone,
+		analitica character varying
+	)
+	language plpgsql
+as $$
+declare
+	declare id_fin bigint;
+begin
+	SELECT lastlogconsulted.registro_id INTO id_fin FROM lastlogconsulted WHERE id=1;
+		
+	UPDATE lastlogconsulted 
+	set registro_id=(
+		SELECT registro_id 
+		FROM log_ds 
+		order by registro_id 
+		desc limit 1) 
+	where id=1;
+		
+	RETURN QUERY SELECT 
+	log_ds.id_cc, 
+	log_ds.id_ds, 
+	log_ds.fps, 
+	to_timestamp(concat(log_ds.fecha, ' ', log_ds.hora), 'DD-MM-YYYY HH24:MI:SS') as datetime,
+	log_ds.analitica
+	FROM log_ds 
+	WHERE registro_id > id_fin
+	ORDER BY registro_id desc;
+end;
+$$
+
 DROP FUNCTION logds_toinflux();
 
 select * from logds_toinflux();
